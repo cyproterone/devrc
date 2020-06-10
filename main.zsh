a6f2c063f05f4a698204c73439128fa60408b315
Fly me to the moon <git@bigly.dog>
2020-06-09 19:49:47 -0700

    mv dev up

--- main.zsh
+++ main.zsh
@@ -4,21 +4,7 @@
 
 _devrc_main() {
   local wd="$(dirname "${(%):-%x}")"
-
   export PATH="$wd/bin:$PATH"
-
-  for module in "$wd/modules/"**(N)
-  do
-    if [[ -d "$module/bin" ]]
-    then
-      export PATH="$module:$PATH"
-    fi
-
-    if [[ -f "$module/main.zsh" ]]
-    then
-      source "$module/main.zsh"
-    fi
-  done
 }
 
 _devrc_main

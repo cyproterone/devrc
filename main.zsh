197d7b5c37a1960e3619d1e40b27e512e3873edd
Fly me to the moon <git@bigly.dog>
2020-06-12 11:43:13 -0700

    npm init

--- main.zsh
+++ main.zsh
@@ -2,11 +2,7 @@
 #################### Main Region ####################
 #################### ########### ####################
 
-_devrc_main() {
-  local wd="$(dirname "${(%):-%x}")"
-  export PATH="$wd/bin:$PATH"
-}
-
-_devrc_main
-unset -f _devrc_main
+export PATH="$XDG_CONFIG_HOME/devrc/bin:$PATH"
+export NPM_GLOBAL_BIN="$XDG_DATA_HOME/npm_global/node_modules/.bin"
+export PATH="$NPM_GLOBAL_BIN:$PATH"
 

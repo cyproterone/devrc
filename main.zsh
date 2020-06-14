3ceb0f071a50654c419ccb875ea28a89fcc1f994
Fly me to the moon <git@bigly.dog>
2020-06-14 13:01:37 -0700

    git ssh

--- main.zsh
+++ main.zsh
@@ -6,3 +6,8 @@ export PATH="$XDG_CONFIG_HOME/devrc/bin:$PATH"
 export NPM_GLOBAL_HOME="$XDG_DATA_HOME/npm_global"
 export PATH="$NPM_GLOBAL_HOME/node_modules/.bin:$PATH"
 
+
+git-ssh() {
+  export GIT_SSH_COMMAND="$(command git-ssh "$@")"
+}
+

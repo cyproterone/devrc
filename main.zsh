66475929d1361c359ea998a262f1155a725d77b8
Fly me to the moon <git@bigly.dog>
2020-06-28 15:03:55 -0700

    mypy

--- main.zsh
+++ main.zsh
@@ -6,6 +6,8 @@ export PATH="$XDG_CONFIG_HOME/devrc/bin:$PATH"
 export NPM_GLOBAL_HOME="$XDG_DATA_HOME/npm_global"
 export PATH="$NPM_GLOBAL_HOME/node_modules/.bin:$PATH"
 
+export MYPY_CACHE_DIR="$XDG_CACHE_HOME/mypy"
+
 
 git-ssh() {
   if [[ $# -eq 0 ]]
